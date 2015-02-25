207212a0f709a6eae8496b55ac10609b2d60e6bc
Gökhan OKUMUŞ <okumusg@windowslive.com>
2015-02-25 14:08:21 +0200

    Update HTTPServer.py

--- HTTPServer.py
+++ HTTPServer.py
@@ -7,9 +7,9 @@ import SocketServer
 
 port = 10000 # Server port 
 
-handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+handler = SimpleHTTPServer.SimpleHTTPRequestHandler # Creating HTTP handler instance
 
-httpd = SocketServer.TCPServer(("",port),handler) # Creating a TCP Server
+httpd = SocketServer.TCPServer(("",port),handler) # Creating a TCP Server with HTTP handler
 
 print "Http Server serving at ", port
 
